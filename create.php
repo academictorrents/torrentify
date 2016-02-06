0cff2e60761c956d584978fc3a0ba299ef6ae7df
henryzlo <henryzlo@gmail.com>
2016-02-06 16:45:40 -0500

    fixed create.php to create pieces appropriately

--- create.php
+++ create.php
@@ -1,133 +1,131 @@
 <?php
 
-/*
- create:
- 1. Takes in GET variable "url", which is url-encoded.
- 2. Creates status file in ./log/ for the download, returns to user in JSON.
- 3. Streamingly creates torrent, updating status as needed.
- 4. When done, dumps torrent file in ./torrents/
-*/
+class Torrent {
+  public $metadata;
+  public $name;
 
-// Convenience functions for torrent making
-function get_piece_size($filesize) {
-  $piece_length = 524288;
-  while (ceil($filesize / $piece_length) == 1) {
-      $piece_length /= 2;
-  }
-  while (ceil($filesize / $piece_length) > 1500) {
-      $piece_length *= 2;
-  }
-  return $piece_length;
-}
+  function __construct ($url_or_file, $id) {
+    $filesize = (int) array_change_key_case(
+      get_headers($url_or_file, TRUE))['content-length'];
+    $piece_size = $this->get_piece_size($filesize);
+    $this->name = end(explode('/', $url_or_file));
+    $log = getcwd() . "/log/" . $id;
 
-function make_pieces($url, $piece_length, $filesize, $logfile) {
-  if (($fp = fopen($url, 'r')) == FALSE) {
-      return FALSE;
+    # now create the actual torrent
+    $this->metadata = array(
+        'announce' => 'http://academictorrents.com/announce.php',
+        'encoding' => 'UTF-8',
+        'info' => array(
+            'length'       => $filesize,
+            'name'         => $this->name,
+            'pieces'       => $this->make_pieces($url_or_file, $piece_size,
+                                                 $filesize, $log),
+            'piece length' => $piece_size,
+        ),
+        'url-list' => array($url_or_file),
+    );
+    $this->save();
   }
-  $pieces = '';
-  $num_pieces = $filesize / $piece_length;
-  for ($i=0; $i<=$num_pieces; $i++) {
-    $log = fopen($logfile, "w");
-    fwrite($log, $i . '/' . (int)$num_pieces);
-    fclose($log);
-    # if last piece, don't read the whole thing
-    if ($i==$num_pieces)
-      $pieces .= pack('H*', sha1(fread($fp, $filesize % $piece_length)));
-    else
-      $pieces .= pack('H*', sha1(fread($fp, $piece_length)));
-  }
-  fclose($fp);
-  return $pieces;
-}
 
-function bencode($var) {
-  if (is_int($var)) {
-    return 'i' . $var . 'e';
-  } else if (is_string($var)) {
-    return strlen($var) . ':' . $var;
-  } else if (is_array($var)) {
-    # must distinguish between dict and list
-    for ($i = 0; $i < count($var); $i++) {
-      # if dict, cannot index using ints?
-      if (!isset($var[$i])) {
-        $dictionary = $var;
-        ksort($dictionary);
-        $ret = 'd';
-        foreach ($dictionary as $key => $value) {
-          $ret .= bencode($key) . bencode($value);
+  static function bencode($var) {
+    if (is_int($var)) {
+      return 'i' . $var . 'e';
+    } else if (is_string($var)) {
+      return strlen($var) . ':' . $var;
+    } else if (is_array($var)) {
+      # must distinguish between dict and list
+      for ($i = 0; $i < count($var); $i++) {
+        # if dict, cannot index using ints?
+        if (!isset($var[$i])) {
+          $dictionary = $var;
+          ksort($dictionary);
+          $ret = 'd';
+          foreach ($dictionary as $key => $value) {
+            $ret .= Torrent::bencode($key) . Torrent::bencode($value);
+          }
+          return $ret . 'e';
         }
-        return $ret . 'e';
+      }
+      $ret = 'l';
+      foreach ($var as $value) {
+        $ret .= Torrent::bencode($value);
+      }
+      return $ret . 'e';
+    }
+  }
+
+  private function get_piece_size($filesize) {
+    $piece_length = 524288;
+    while (ceil($filesize / $piece_length) == 1) {
+        $piece_length /= 2;
+    }
+    while (ceil($filesize / $piece_length) > 1500) {
+        $piece_length *= 2;
+    }
+    return $piece_length;
+  }
+
+  private function make_pieces($url, $piece_length, $filesize, $logfile) {
+    if (($fp = fopen($url, 'r')) == FALSE) {
+        return FALSE;
+    }
+    $pieces = '';
+    $part = '';
+
+    $position = 0;
+    while ($position < $filesize) {
+      $bytes_read = 0;
+      # fread doesn't actually read in the correct number of bytes
+      # piece together multiple freads
+      while ($bytes_read < $piece_length && $position < $filesize) {
+        $this_part = fread($fp, min($piece_length, $filesize - $position));
+        $bytes_read += strlen($this_part);
+        $position += strlen($this_part);
+        $part .= $this_part;
+      }
+      $next_part = substr($part, $piece_length);
+      $part = substr($part, 0, $piece_length);
+      $piece = sha1($part, $raw_output=TRUE);
+      $pieces .= $piece;
+
+      $part = $next_part;
+
+      if ($position > $filesize) {
+        $position = $filesize;
+      }
+      # log progress every 5 pieces
+      if ($position == $filesize || $position % (5*$piece_length) == 0) {
+        $log = fopen($logfile, "w");
+        fwrite($log, $position . '/' . $filesize);
+        fclose($log);
       }
     }
-    $ret = 'l';
-    foreach ($var as $value) {
-      $ret .= bencode($value);
-    }
-    return $ret . 'e';
+    fclose($fp);
+    return $pieces;
   }
-  # should throw some kind of error here
-  #throw new InvalidArgumentException('Type ' . gettype($var) . ' can not be encoded.');
+
+  private function save() {
+    $f = fopen(getcwd() . "/torrents/" . $this->name . ".torrent", "w");
+    fwrite($f, Torrent::bencode($this->metadata));
+    fclose($f);
+  }
 }
 
-# Main
-
-# this validates the URL
-$url = urldecode($_GET["url"]);
-if (isset($_GET["url"]) && filter_var($url, FILTER_VALIDATE_URL)) {
-  $length = (int) array_change_key_case(
-    get_headers($url, TRUE))['content-length'];
-  $piece_size = get_piece_size($length);
-  $name = end(explode('/', $url));
-  $log = tempnam(getcwd() . "/log", "stat");
-
-  # stuff to close connection early
-  ignore_user_abort(true); // just to be safe
-  ob_start();
-
-  # return the log ID, for querying later
-  echo(json_encode(array(
-    'status' => 'OK',
-    'id' => end(explode('/', $log)))));
-
-  $contents = ob_get_contents();
-  $len = strlen($contents);
-  header("Connection: close");
-  header("Content-Length: $len");
-  echo $contents;
-  flush();
-
-  # now create the actual torrent
-  $torrent = array(
-      'announce' => 'http://academictorrents.com/announce.php',
-      'encoding' => 'UTF-8',
-      'info' => array(
-          'length'       => $length,
-          'name'         => $name,
-          'pieces'       => make_pieces($url, $piece_size, $length, $log),
-          'piece length' => $piece_size,
-      ),
-      'url-list' => array($url),
-  );
-  $torrent = bencode($torrent);
-
-  # done creating, write the torrent file
-  $f = fopen(getcwd() . "/torrents/" . $name . ".torrent", "w");
-  fwrite($f, $torrent);
-  fclose($f);
-
 /*
-  # make the download
-  header('Content-Description: File Transfer');
-  header('Content-Type: application/octet-stream');
-  header('Content-Disposition: attachment; filename="' . $name . '.torrent"');
-  header('Expires: 0');
-  header('Cache-Control: must-revalidate');
-  header('Pragma: public');
-  echo $torrent;
-  exit;*/
+ create:
+ 1. Input: GET variables url-encoded string "url", random int "id".
+ 2. Creates log for "id" in ./log/ for the download.
+ 3. Streamingly creates torrent, updating log.
+ 4. Dumps finished torrent file in ./torrents/
+*/
+
+# Main
+if (isset($_GET["url"]) && isset($_GET["id"]) &&
+    filter_var($_GET["url"], FILTER_VALIDATE_URL)) {
+  $torrent = new Torrent($_GET["url"], $_GET["id"]);
 } else {
   echo(json_encode(array(
     'status' => 'ERROR',
-    'msg' => "Type in a URL!")));
+    'msg' => "Requires GET variables 'url' and 'id'")));
 };
 ?>
